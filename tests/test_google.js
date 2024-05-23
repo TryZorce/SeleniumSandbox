7c3067605ea4b3b775d6a1813c15baa5af59490f
TryZorce <christopher.moron@gmail.com>
2024-05-23 11:01:18 +0200

    Exercice Google OK

---
+++ tests/test_google.js
@@ -0,0 +1,37 @@
+const { By, Key, Builder, WebElementCondition, until } = require("selenium-webdriver");
+const assert = require("assert");
+
+(async function test_function() {
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    try {
+        await driver.get('https://www.google.com');
+
+        let acceptButton = await driver.wait(until.elementLocated(By.css('.QS5gu.sy4vM')), 5000);
+        if (acceptButton) {
+            await acceptButton.click();
+        } else {
+            console.log('Bouton tout accepter pas trouvée');
+        }
+
+        let searchBox = await driver.wait(until.elementLocated(By.name('q')), 10000);
+        await searchBox.sendKeys('Selenium WebDriver', Key.RETURN);
+
+        await driver.wait(until.titleContains('Selenium WebDriver'), 10000);
+        let title = await driver.getTitle();
+        assert(title.includes('Selenium WebDriver'));
+
+    } catch (e) {
+        console.log(e);
+
+    } finally {
+        await driver.quit();
+
+        /*
+        setInterval(function(){
+            driver.quit();
+        }, 10000);
+        */
+
+    }
+})()
